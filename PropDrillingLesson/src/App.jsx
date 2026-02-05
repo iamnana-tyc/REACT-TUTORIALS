93e1684a36368649f1e742f3ab16dc2c30458094
iamnana-tyc <nanatyc7@gmail.com>
2026-02-05 14:43:50 +0300

    Understanding context scope

--- PropDrillingLesson/src/App.jsx
+++ PropDrillingLesson/src/App.jsx
@@ -1,25 +1,33 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const ThemeContext = createContext('light');
 
 function App()
 {
-  //const myTheme = 'dark';
+  const [theme, setTheme] = useState('blue');
+  const toggleTheme = () =>
+  {
+    setTheme((prevTheme) => (prevTheme === 'blue' ? 'dark' : 'blue'));
+  };
 
   return (
-    <ThemeContext.Provider value="red">
-      <div style={{ border: '2px solid black', padding: '10px' }}>
-        <h2>App (Parent)</h2>
-        <ComponentA />
-      </div>
-    </ThemeContext.Provider>
+    <div>
+      <GlobalComponent />
+      <ThemeContext.Provider value={theme}>
+        <div style={{ border: '2px solid black', padding: '10px' }}>
+          <h2>App (Parent)</h2>
+          <button onClick={toggleTheme}>Toggle Theme</button>
+          <ComponentA />
+        </div>
+      </ThemeContext.Provider>
+    </div>
   );
 };
 
 function ComponentA()
 {
   return (
-    <div style={{ border: '2px solid black', padding: '10px' }}>
+    <div style={{ border: '2px solid green', padding: '10px' }}>
       <h2>ComponentA (Child)</h2>
       <ComponentB />
     </div>
@@ -30,7 +38,7 @@ function ComponentA()
 function ComponentB()
 {
   return (
-    <div style={{ border: '2px solid black', padding: '10px' }}>
+    <div style={{ border: '2px solid gold', padding: '10px' }}>
       <h2>ComponentB (GrandChild)</h2>
       <ThemedComponent />
     </div>
@@ -42,7 +50,7 @@ function ThemedComponent()
 {
   const myTheme = useContext(ThemeContext);
   return (
-    <div style={{ border: '2px solid black', padding: '10px' }}>
+    <div style={{ border: '2px solid red', padding: '10px' }}>
       <h2>ThemedComponent (Great-GrandChild)</h2>
       <div>The current theme is: {myTheme}</div>
     </div>
@@ -50,5 +58,16 @@ function ThemedComponent()
   );
 };
 
+function GlobalComponent()
+{
+  const myTheme = useContext(ThemeContext);
+  return (
+    <div style={{ border: '2px solid black', padding: '10px' }}>
+      <h2>GlobalComponent (Outside Provider)</h2>
+      <div>The current theme is: {myTheme}</div>
+    </div>
+
+  );
+};
 
 export default App;
