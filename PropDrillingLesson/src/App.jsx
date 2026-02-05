f52715629d5bc10f5a029ddce0cb0b4aa5c43d88
iamnana-tyc <nanatyc7@gmail.com>
2026-02-05 14:04:29 +0300

    Understanding Prop drilling with useContext

---
+++ PropDrillingLesson/src/App.jsx
@@ -0,0 +1,53 @@
+import { createContext, useContext } from "react";
+
+const ThemeContext = createContext('light');
+
+function App()
+{
+  //const myTheme = 'dark';
+
+  return (
+    <div style={{ border: '2px solid black', padding: '10px' }}>
+      <h2>App (Parent)</h2>
+      <ComponentA />
+    </div>
+
+  );
+};
+
+function ComponentA()
+{
+  return (
+    <div style={{ border: '2px solid black', padding: '10px' }}>
+      <h2>ComponentA (Child)</h2>
+      <ComponentB />
+    </div>
+
+  );
+};
+
+function ComponentB()
+{
+  return (
+    <div style={{ border: '2px solid black', padding: '10px' }}>
+      <h2>ComponentB (GrandChild)</h2>
+      <ThemedComponent />
+    </div>
+
+  );
+};
+
+function ThemedComponent()
+{
+  const myTheme = useContext(ThemeContext);
+  return (
+    <div style={{ border: '2px solid black', padding: '10px' }}>
+      <h2>ThemedComponent (Great-GrandChild)</h2>
+      <div>The current theme is: {myTheme}</div>
+    </div>
+
+  );
+};
+
+
+export default App;
